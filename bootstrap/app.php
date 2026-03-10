2b3fff72c76fda3f18c5c994c10d94a9bbc5084d
Amit Kumar Chandrakar <amitchandrakar028@gmail.com>
2026-03-10 16:22:31 +0530

    Fix: cors issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,6 @@ use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
